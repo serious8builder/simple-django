5189bb3018ae67c8a61e562a200d6020fecca10a
RK <rk@hpcnt.com>
2020-12-10 21:48:48 +0900

    Add gunicorn config file

---
+++ gunicorn_config.py
@@ -0,0 +1,20 @@
+import os
+
+bind = '0.0.0.0:' + os.environ.get('SIMPLE_DJ_PORT', '5000')
+max_requests = 10
+num_cpu = os.cpu_count()
+workers = int(os.getenv('SIMPLE_DJ_WORKERS') or 2 * num_cpu + 1)
+
+worker_connections = 10
+timeout = 60
+keepalive = 2
+
+os.makedirs('/tmp/simple-dj', exist_ok=True)
+pidfile = "/tmp/simple-dj/simple-dj.pid"
+logfile = "/tmp/simple-dj/simple-dj.log"
+loglevel = "debug"
+errorlog = '-'
+accesslog = '-'
+
+proc_name = "simple-dj"
+preload_app = True
