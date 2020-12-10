f6e1d2f53edfdb9b00c26f172b77346d00cc9ec8
RK <rk@hpcnt.com>
2020-12-10 21:34:50 +0900

    Setting: SITE_ID =1
    Change url for login

--- simple_dj/urls.py
+++ simple_dj/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='login/index.html')),
+    path('login/', TemplateView.as_view(template_name='login/index.html')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ]
